485b8f350f1d56c00461e9d2384a080f467d580f
Oli <o.clivegriffin@gmail.com>
2022-03-23 00:23:22 +1300

    wip

--- src/components/project-tile.tsx
+++ src/components/project-tile.tsx
@@ -2,40 +2,37 @@ import { FC } from 'react'
 import { Project } from '../service/project-service'
 
 
-const ProjectTile: FC<Project> = ({ title, description }) => {
+const ProjectTile: FC<Project> = ({ title, description, tags, participants }) => {
 
-  const users = [
-    { name: 'Oli' },
-    { name: 'Annie' },
-    { name: 'Gallgher' },
-    { name: 'j' },
-  ]
-
-  const tags = [ 'asdf', 'rete', 'cxv' ]
+  const harshShadow: React.CSSProperties = {
+    boxShadow: '10px 10px 0px 1px rgba(0,0,0,1)',
+    WebkitBoxShadow: '10px 10px 0px 1px rgba(0,0,0,1)',
+  }
 
   return (
-    <div className='rounded-md shadow-lg p-8 bg-white'>
+    <div style={harshShadow} className='border-2 border-slate-800 p-8 bg-white harsh-shadow'>
       <p className='text-3xl font-semibold'>
         {title}
       </p>
       <div className='h-2' />
       <div className='flex flex-row items-center'>
-        {tags.map(tag => (
+        {tags?.map(({ label }) => (
           <>
             <div className='py-0.5 px-2 rounded-sm bg-blue-200' >
-              <p className='text-xs text-blue-500'>{tag}</p>
+              <p className='text-xs text-blue-500'>{label}</p>
             </div>
             <div className="w-1" />
           </>
         ))}
       </div>
+      <div className='h-4' />
       <p>
         {description}
       </p>
       <div className='flex flex-row pt-4 items-center'>
-        {users.map(u => (
+        {participants?.map(({ firstName }) => (
           <div className='flex items-center justify-center aspect-square w-6 rounded-full shadow-md ring-2 ring-white bg-blue-300'>
-            <p className='text-white text-sm'>{u.name[0]}</p>
+            <p className='text-white text-sm'>{firstName[0]}</p>
           </div>
         ))}
         <div className='w-4' />
--- src/pages/universal/layout.tsx
+++ src/pages/universal/layout.tsx
@@ -8,8 +8,8 @@ const Layout: FC = ({ children }) => {
 
   return (
     <div>
-      <div className='relative z-50 flex flex-row h-16 shadow-md shadow-gray-200 items-center justify-between px-24'>
-        <div className="bg-yellow-300 py-1 px-2 outline outline-2 outline-slate-700 rounded-sm">
+      <div className='border border-b-2 border-slate-800 relative z-50 flex flex-row h-16  items-center justify-between px-24'>
+        <div className="h-full px-8 flex bg-purple-300 py-1 px-2 outline outline-2 outline-slate-700 items-center">
           <p className='text-xl font-bold text-slate-800'>HackerHouse</p>
         </div>
         <div>
